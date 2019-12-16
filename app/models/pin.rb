e70d7bd2b80e94f5ad2943fd61deecb527afd3ab
Mathieu Clément <mcfly1893@gmail.com>
2019-12-16 13:25:37 +0100

    Add home page

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -3,4 +3,8 @@ class Pin < ApplicationRecord
   validates :image_url, presence: true
   validates :tag, length: { maximum: 30 }
   belongs_to :user
+
+  def self.most_recent
+    all.order(created_at: :desc).limit(6)
+  end
 end
