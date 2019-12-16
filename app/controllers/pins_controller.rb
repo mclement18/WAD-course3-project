86b1968e9895c17e88876a285cd7b3041fb50b28
Mathieu Clément <mcfly1893@gmail.com>
2019-12-16 10:48:36 +0100

    Add content to pins/new view and add index, new and create PinsController actions

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,2 +1,18 @@
 class PinsController < ApplicationController
+  def index
+  end
+
+  def new
+    @pin = Pin.new
+  end
+
+  def create
+    @pin = Pin.new(pin_params)
+    @pin.user = User.find(session[:user_id])
+    if @pin.save
+      redirect_to pins_path
+    else
+      render 'new'
+    end
+  end
 end
