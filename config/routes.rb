76a9b07ea88cb2d44c6f1232c04be0aa443b7e0e
Mathieu Clément <mcfly1893@gmail.com>
2019-12-16 10:32:32 +0100

    Generate PinsController and resources routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get 'about/index'
   
   resources :users
+
+  resources :pins
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
