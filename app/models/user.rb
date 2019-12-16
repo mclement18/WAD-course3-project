dbe95eae9a8bdf2b2cb7a6866519c63769d531a8
Mathieu Clément <mcfly1893@gmail.com>
2019-12-16 10:29:16 +0100

    Associate User to Pin, one to many association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
   validates :email, presence: true, email: true, uniqueness: true
+  has_many :pins
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191216092225) do
+ActiveRecord::Schema.define(version: 20191216092638) do
 
   create_table "pins", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 20191216092225) do
     t.string "tag"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_pins_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
