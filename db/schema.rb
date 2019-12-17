1d5abd9b3574cf7d579a12659fc584fbbe4b7781
Mathieu Clément <mcfly1893@gmail.com>
2019-12-17 19:25:16 +0100

    Generate Comment model with pin and user references

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191216092638) do
+ActiveRecord::Schema.define(version: 20191217182321) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "body"
+    t.integer "pin_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["pin_id"], name: "index_comments_on_pin_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "pins", force: :cascade do |t|
     t.string "title"
